048920de258dd1b6c1f23c4e76e3e1d39a439df2
MacLean Hawley <macleanhawley@gmail.com>
2023-09-01 19:28:56 +0200

    Change team membership structure

--- src/main/kotlin/com/pollywog/teams/Team.kt
+++ src/main/kotlin/com/pollywog/teams/Team.kt
@@ -1,13 +1,29 @@
 package com.pollywog.teams
 
 import com.pollywog.tokens.Token
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class Team(
-    val members: List<String>,
+    val members: Map<String, Membership>,
     val activeTokens: List<Token> = emptyList(),
     val revokedTokens: List<Token> = emptyList(),
     val name: String,
     val secrets: Map<String, String> = emptyMap()
 )
+
+enum class Membership {
+
+    @SerialName("admin")
+    ADMIN,
+
+    @SerialName("editor")
+    EDITOR,
+
+    @SerialName("tester")
+    TESTER,
+
+    @SerialName("viewer")
+    VIEWER,
+}

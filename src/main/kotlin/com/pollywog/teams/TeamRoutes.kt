9ba1df2a5398b848d3f26424360c3a054db591bf
MacLean Hawley <macleanhawley@gmail.com>
2023-09-05 17:05:09 +0200

    Fix team membership strcuture & secret managmement

--- src/main/kotlin/com/pollywog/teams/TeamRoutes.kt
+++ src/main/kotlin/com/pollywog/teams/TeamRoutes.kt
@@ -10,6 +10,9 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class AddSecretRequest(val secret: String, val key: String)
+
+@Serializable
+data class DeleteSecretRequest(val key: String)
 fun Route.teamRouting(teamService: TeamService) {
     authenticate("auth-bearer") {
         post("team/{id?}/secret/add") {
@@ -21,6 +24,18 @@ fun Route.teamRouting(teamService: TeamService) {
             val userIdPrincipal = call.principal<UserIdPrincipal>() ?: return@post call.respond(HttpStatusCode.Unauthorized)
 
             teamService.addSecret(requestBody.secret, requestBody.key, teamId, userIdPrincipal.name)
+            call.respondText("Added ${requestBody.key}")
+        }
+
+        delete("team/{id?}/secret/delete") {
+            val teamId = call.parameters["id"] ?: return@delete call.respondText(
+                "Missing id",
+                status = HttpStatusCode.BadRequest
+            )
+            val requestBody = call.receive<DeleteSecretRequest>()
+            val userIdPrincipal = call.principal<UserIdPrincipal>() ?: return@delete call.respond(HttpStatusCode.Unauthorized)
+
+            teamService.deleteSecret(requestBody.key, teamId, userIdPrincipal.name)
             call.respondText("nice")
         }
     }
--- src/main/kotlin/com/pollywog/teams/TeamService.kt
+++ src/main/kotlin/com/pollywog/teams/TeamService.kt
@@ -18,4 +18,16 @@ class TeamService (
 
         teamRepository.update(teamRepoIdProvider.id(teamId), mapOf("secrets.$key" to encrypted))
     }
+
+    suspend fun deleteSecret(key: String, teamId: String, userId: String) {
+        val team = teamRepository.get(teamRepoIdProvider.id(teamId)) ?: throw Exception("No team $teamId")
+
+        if (!team.members.contains(userId)) {
+            throw Exception("You're not a member of this team")
+        }
+
+        val updatedSecrets = team.secrets.filter { it.key != key }
+
+        teamRepository.update(teamRepoIdProvider.id(teamId), mapOf("secrets" to updatedSecrets))
+    }
 }

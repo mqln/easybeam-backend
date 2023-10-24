9ed0638eda92f9cf818bcbe24c7bbf9eb5347b99
MacLean Hawley <macleanhawley@gmail.com>
2023-10-24 22:04:16 +0200

    use shared JSON object

--- src/main/kotlin/com/pollywog/common/RedisRepository.kt
+++ src/main/kotlin/com/pollywog/common/RedisRepository.kt
@@ -1,6 +1,7 @@
 package com.pollywog.common
 
 
+import com.pollywog.plugins.sharedJson
 import kotlinx.serialization.KSerializer
 import kotlinx.serialization.json.Json
 import kotlinx.serialization.json.decodeFromJsonElement
@@ -16,7 +17,7 @@ class RedisRepository<T>(
         val data = jedis.get(id) ?: return null
 
         // Deserialize the data using kotlinx.serialization
-        return Json.decodeFromString(serializer, data)
+        return sharedJson.decodeFromString(serializer, data)
     }
 
     override suspend fun set(id: String, data: T) {

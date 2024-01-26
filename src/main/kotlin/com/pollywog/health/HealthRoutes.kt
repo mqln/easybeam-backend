9542e91653861af35107c6ab94c5e0d02bbbed7c
MacLean Hawley <macleanhawley@gmail.com>
2024-01-26 19:57:24 +0100

    expose overview

--- src/main/kotlin/com/pollywog/health/HealthRoutes.kt
+++ src/main/kotlin/com/pollywog/health/HealthRoutes.kt
@@ -6,11 +6,20 @@ import io.ktor.server.routing.*
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class HealthResponse(val internal: Map<String, HealthStatus>, val external: Map<String, HealthStatus>)
+data class HealthResponse(
+    val internalOverall: HealthStatus,
+    val internal: Map<String, HealthStatus>,
+    val externalOverall: HealthStatus,
+    val external: Map<String, HealthStatus>
+)
 
 fun Route.healthRouting(healthService: HealthService) {
     get("health") {
         val health = healthService.healthCheck()
-        call.respond(HealthResponse(internal = health.internal, external = health.external))
+        call.respond(HealthResponse(
+            internalOverall = health.internalOverall,
+            internal = health.internal,
+            externalOverall = health.externalOverall,
+            external = health.external))
     }
 }
